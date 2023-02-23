fb6bb93bf33bbb753a7ed5150b68a12ef79bf0de
aluno local <alunoLocal@IPIRANGA.FATEC>
2023-02-23 18:11:58 -0300

    Remove .class file

--- TesteEmpregado.java
+++ TesteEmpregado.java
@@ -4,20 +4,20 @@ public class TesteEmpregado {
         Empregado e1 = new Empregado1();
         Empregado e2 = new Empregado2();
 
-        e0.setSalario(1);
-        e0.setComissao(1);
-        e0.setBonus(1);
+        e0.setSalario(2);
+        e0.setComissao(2);
+        e0.setBonus(2);
 
-        e1.setSalario(1);
-        e1.setComissao(1);
-        e1.setBonus(1);
+        e1.setSalario(2);
+        e1.setComissao(2);
+        e1.setBonus(2);
 
-        e2.setSalario(1);
-        e2.setComissao(1);
-        e2.setBonus(1);
+        e2.setSalario(2);
+        e2.setComissao(2);
+        e2.setBonus(2);
         
-        System.out.println("salario do empregado0" + String.valueOf(e0.calcularSalarioTotal()));
-        System.out.println("salario do empregado1" + String.valueOf(e1.calcularSalarioTotal()));
-        System.out.println("salario do empregado2" + String.valueOf(e2.calcularSalarioTotal()));
+        System.out.println("salario do empregado0: " + String.valueOf(e0.calcularSalarioTotal()));
+        System.out.println("salario do empregado1: " + String.valueOf(e1.calcularSalarioTotal()));
+        System.out.println("salario do empregado2: " + String.valueOf(e2.calcularSalarioTotal()));
     }
 }
