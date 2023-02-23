2d0ff73f0903cffdce373c726e45a705b58ce5ea
aluno local <alunoLocal@IPIRANGA.FATEC>
2023-02-23 17:22:10 -0300

    Adiciona calculo de salario

--- Empregado.java
+++ Empregado.java
@@ -1,6 +1,22 @@
 public class Empregado{
+    int tipo;
+
     private String name;
     private int age;
+    private double salario, comissao, bonus;
+
+    public double calcularSalario(){
+        switch(tipo){
+            case 0:
+                return salario;
+            case 1:
+                return salario + salario * comissao;
+            case 2:
+                return salario + bonus;
+            default:
+                return 0;
+        }
+    };
 
     public String getName() {return name;}
     public void setName(String name) {this.name = name;};
